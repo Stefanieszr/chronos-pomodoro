49cc3539fe53095a9086932da735d1fb62939203
Stefanie Rodrigues <stefaniesouzad18@gmail.com>
2025-04-13 20:04:06 -0300

    Seção 2 - Aula 3 até 28

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/theme.css';
+import './styles/global.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Container } from './components/Container/index';
+import { Logo } from './components/Logo/index';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Container>
+        <Logo></Logo>
+      </Container>
 
-export default App
+      <Container>
+        <Menu></Menu>
+      </Container>
+
+      <Container>
+        <CountDown></CountDown>
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='inputTask'
+              type='text'
+              title='tarefa'
+              placeholder='Digite algo'
+            ></DefaultInput>
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Enviar</button>
+          </div>
+        </form>
+      </Container>
+    </>
+  );
+}
